8625a4d2cc22a61f73c32decdf9e3108fb3de009
Aaron Macken <aaron.a.macken@gmail.com>
2019-07-08 15:58:33 -0700

    Added error handling to the fetch statement.

--- index.js
+++ index.js
@@ -26,13 +26,31 @@ var price = document.querySelector("#price");
 var URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 btn.addEventListener("click", function() {
+  // perform a get request using fetch & the given url
   fetch(URL)
+  // first check for errors
+    .then(handleErrors)
+    // if last statement is cleared, return the promise object as json
     .then(function(response) {
-      console.log(response);
+      console.log("Everything is fine.");
       return response.json();
     })
+    // then use the json to load in the data we want from the fetch api request
     .then(function(data) {
       console.log(data.bpi.USD.rate);
       price.innerHTML = ("$" + data.bpi.USD.rate);
-    });
+    })
+    // catch block to write error message to the console.
+    .catch(function(error) {
+      console.log(error);
+    })
 });
+
+
+// This fn is used to handle any type of status error
+function handleErrors(request) {
+  if(!request.ok) {
+    throw Error(request.status);
+  }
+  return request;
+}
