7b35e51051d62973064d0218e5acf0e791c6bedd
Aaron Macken <aaron.a.macken@gmail.com>
2019-07-08 15:00:13 -0700

    Modified the code to use fetch instead of XHR.

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 var btn = document.querySelector("button");
 var price = document.querySelector("#price");
 
+// Old way with XHR
+
+// btn.addEventListener("click", function() {
+//   var XHR = new XMLHttpRequest();
+
+//   XHR.onreadystatechange = function() {
+//     if(XHR.readyState == 4 && XHR.status == 200) {
+//       var data = JSON.parse(XHR.responseText);
+//       var usd = data.bpi.USD.rate;
+//       var eur = data.bpi.EUR.rate;
+//       var gbp = data.bpi.GBP.rate;
+//       price.innerHTML = "$" + usd;
+//     }
+//   }
+
+//   XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+//   XHR.send();
+
+// });
+
+// New way with fetch
+
+var URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
 btn.addEventListener("click", function() {
-  var XHR = new XMLHttpRequest();
-  
-  XHR.onreadystatechange = function() {
-    if(XHR.readyState == 4 && XHR.status == 200) {
-      var data = JSON.parse(XHR.responseText);
-      var usd = data.bpi.USD.rate;
-      var eur = data.bpi.EUR.rate;
-      var gbp = data.bpi.GBP.rate;
-      price.innerHTML = "$" + usd;
-    }
-  }
-  
-  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
-  XHR.send();
-  
-  
+  fetch(URL)
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data.bpi.USD.rate);
+      price.innerHTML = ("$" + data.bpi.USD.rate);
+    });
 });
